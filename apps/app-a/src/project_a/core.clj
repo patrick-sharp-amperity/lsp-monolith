b6e7985f2dac2f1d60b013921f6ae556bc323304
Patrick Sharp <patrick.sharp@amperity.com>
2023-01-18 17:50:55 -0800

    add more references

--- apps/app-a/src/project_a/core.clj
+++ apps/app-a/src/project_a/core.clj
@@ -2,8 +2,6 @@
   (:require
     [lein-monolith.example/lib-a :as lib-a]))
 
-(defn bar [x] (lib-a/foo x))
-
 (defn foo
   "I don't do a whole lot."
   [x]
@@ -12,3 +10,5 @@
 (foo "I say: ")
 (foo "I say again: ")
 
+(lib-a/foo "They say: ")
+(lib-a/foo "They say again: ")
